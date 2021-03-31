ca46afde1d0944135f64c569bf4d4184f229c54f
efennie <efennie@email.arizona.edu>
2021-03-31 11:09:19 -0700

    created pseudo code for pybank

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+#import the csv file and read it
+
+#total count of column 0 to get total # of months in the set
+    #store as variable "total_months"
+
+#sum of column 2 to get the net total profits/losses
+    #store as variable "total_p_l"
+
+#calculate average change over time
+    #it'll have to be the average of (second month - first month)/total month
+    # sum of change divided by total months
+    #store average change as "av_change"
+
+#calculate greatest increase month
+    #store as a variable
+    #find a way to associate greatest inc with month included
+
+#calculate greatest losses month
+    #store as a variable
+    #find a way to associate greatest loss with month included
+
+#assign report as a def
+    #print("Total Months: " + total_months) try str(total_months) if the first way doesn't work
+    #print("Total: " + total_p_l)
+    #print("Average Change: " + av_change)
+    #print("Greatest Increase in Profits: " + column 0 + column1)
+
+#print(report)
+
+#export report as txt file
