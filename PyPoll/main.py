dd8d2af1cbfbf46776ce15a174241cc5451ea062
efennie <efennie@email.arizona.edu>
2021-04-01 22:53:22 -0700

    wrote out pseudocode for pypoll

---
+++ PyPoll/main.py
@@ -0,0 +1,18 @@
+#import dependencies
+import os
+import csv
+
+#create path and import csv file and read the file
+csvpath = os.path.join('PyPoll', 'Resources', 'Python_PyPoll_Resources_election_data.csv')
+
+#calculate the total number of votes cast
+
+
+#create a complete list of candidates who received votes
+
+#calculate the percentage of the total vote each candidate won
+
+#calculate the total number of votes candidate won
+
+#determine the winner of the election based on popular vote 
+# & print "Winner: " + their name
